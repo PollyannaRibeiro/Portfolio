42a100e9851b62c047a8df39eed9c4ea8b50cd9f
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2021-06-22 12:54:34 +0100

    Design Experience - content page added

---
+++ src/components/DesignExperience.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import SkillsMenu from '../components/DesingSkillsMenu';
+import SoleBeauty from '../pages/SoleBeauty';
+import EthosData from '../pages/EthosData';
+import Freelas from '../pages/Freelance';
+
+function DesignExperience(){
+  return(
+    <>
+      <div className="container" id="design-projects"> {/* container */}
+
+        <h1>Design Experience</h1>
+        <p>I have experience in creating responsive and accessible websites (HTML, CSS, JS, React, Bootstrap, and Aria), 
+          supporting/maintaining WordPress and Wix websites. Creating advertisement material for social media, google ads, 
+          including animated content, retouching, and video edition.</p>
+        <p>Below, you can find some works that I've done.</p>
+        
+        <div>
+          {/* Skills Menu */}
+          <Router>
+            <SkillsMenu/>
+            <Switch>
+              <Route path="/solebeauty"><SoleBeauty/></Route>
+              <Route path="/ethosdata"><EthosData/></Route>
+              <Route path="/freelance"><Freelas/></Route>
+            </Switch>
+          </Router>
+        </div>
+      </div> {/* end of container */}
+
+    </>
+  )
+}
+
+export default DesignExperience
